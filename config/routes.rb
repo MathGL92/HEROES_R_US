e5f354b2c402bac45524e9f55c9b8d20d5337eba
Matt McCrystal <mccrystal.matthew@gmail.com>
2020-09-08 18:07:28 +1000

    user dashboard with bookings. They can be accepted or declined

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :heroes, only: [:index, :show] do
-    resources :bookings, only: [:create]
+  resources :heroes, only: [:new, :create, :index, :show] do
+    resources :bookings, only: [:create, :update]
   end
   
+  resources :users, only: [:show] do
+    resources :dashboard, only: [:index]
+  end
+ 
 end
