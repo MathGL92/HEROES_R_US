b25783c3c19ee4977ab770d9fcc25696df257b20
Lily Tan <lilyt2995@gmail.com>
2020-09-09 12:49:58 +1000

    updated seeds, heroes can now be edited

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,17 +1,32 @@
 class HeroesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :set_hero, only: [:show]
+  before_action :set_hero, only: [:show, :edit, :update]
   def index
     @heroes = Hero.all
+  end
+
+  def edit
+  end
+  
+  def update
+    @hero.update(hero_params)
+
+    redirect_to hero_path(@hero)
   end
 
   def show
     @booking = Booking.new
   end
 
+
+
   private
 
   def set_hero
     @hero = Hero.find(params[:id])
+  end
+
+  def hero_params
+    params.require(:hero).permit(:name, :power, :description, :address, :price, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :heroes, only: [:new, :create, :index, :show] do
+  resources :heroes, only: [:new, :edit, :update, :create, :index, :show] do
     resources :bookings, only: [:create, :update]
   end
   
