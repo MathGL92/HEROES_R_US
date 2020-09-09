ae3a2ae39003043535435e757b688712f7eb4529
Bence Fulop <fbence90@gmail.com>
2020-09-09 12:52:22 +1000

    new page progress

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,6 +1,22 @@
 class HeroesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_hero, only: [:show]
+  
+  def new
+    @hero = Hero.new
+  end
+
+  def create
+    @hero = Hero.new(hero_params)
+    @hero.user = current_user
+    if @hero.save
+      redirect_to root_path
+    else
+      render :new
+    end
+    # raise
+  end
+  
   def index
     @heroes = Hero.all
   end
@@ -14,4 +30,12 @@ class HeroesController < ApplicationController
   def set_hero
     @hero = Hero.find(params[:id])
   end
+
+  def hero_params
+    params.require(:hero).permit(:name, :power, :description, :address, :price)
+  end
+
+ 
+
+
 end
