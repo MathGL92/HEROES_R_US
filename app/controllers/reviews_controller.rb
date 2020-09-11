29d6bca005c296249530496ad87338e25bc16d64
Mathieu Longe <mathieu.longe@orange.fr>
2020-09-11 12:59:01 +1000

    modal done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,18 @@
 class ReviewsController < ApplicationController
   def new
-      @booking = Booking.find(params[:booking_id])
-      @review = Review.new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
   end
 
   def create
-      # @booking = @booking.review
-      @review = Review.new(review_params)
-      @review.booking = Booking.find(params[:booking_id])
-      if @review.save
-        redirect_to user_dashboard_index_path(current_user)
-      else
-        render :new
-      end
+    # @booking = @booking.review
+    @review = Review.new(review_params)
+    @review.booking = Booking.find(params[:booking_id])
+    if @review.save
+      redirect_to user_dashboard_index_path(current_user)
+    else
+      render :new
+    end
   end
 
   private
